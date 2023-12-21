0ceb12aa715ec7c36b1e420dfc92c674cd185d1f
romashevaa <149483099+romashevaa@users.noreply.github.com>
2023-12-21 03:41:29 +0200

    Add files via upload

--- lab5/app.py
+++ lab5/app.py
@@ -1,13 +1,14 @@
-import datetime
 import os
 import json
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.secret_key = 'sheva'
+csrf = CSRFProtect(app)
 
 
 def load_users():
@@ -18,6 +19,9 @@ def load_users():
 
 users = load_users()
 user_cookies = {}
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 
 class LoginForm(FlaskForm):
@@ -53,10 +57,12 @@ def login():
 @app.route('/info')
 def info():
     username = session.get('username', None)
-    if username:
-        user_info = users.get(username, None)
-        if user_info:
-            return render_template('info.html', user_info=user_info)
+    user_info = users.get(username, None)
+
+    if user_info:
+        return render_template('info.html', user_info=user_info)
+
+    flash('User not found. Please log in.', 'error')
     return redirect(url_for('login'))
 
 
@@ -72,39 +78,48 @@ def logout():
 @app.route('/add_cookie', methods=['POST'])
 def add_cookie():
     username = session.get('username', None)
+
     if username:
         key = request.form['cookie_key']
         value = request.form['cookie_value']
         user_cookies[key] = {"value": value,
                              "creation_time": str(datetime.datetime.now())}
+
     return redirect(url_for('info'))
 
 
 @app.route('/delete_cookie', methods=['POST'])
 def delete_cookie():
     username = session.get('username', None)
+
     if username:
         key = request.form['delete_cookie_key']
+
         if key in user_cookies:
             user_cookies.pop(key)
+
     return redirect(url_for('info'))
 
 
 @app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
     username = session.get('username', None)
+
     if username:
         if request.method == 'POST':
             new_password = request.form.get('new_password')
+
             if new_password:
                 change_user_password(username, new_password)
                 flash('Password changed successfully!', 'success')
+
     return redirect(url_for('info'))
 
 
 def change_user_password(username, new_password):
     with open('static/clients.json', 'r+') as f:
         users = json.load(f)
+
         if username in users:
             users[username]['password'] = new_password
             f.seek(0)
